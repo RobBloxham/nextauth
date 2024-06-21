ddd54fd8faf969fac456fd4cf7d7bc40082f35d7
RobBloxham <Rob.Bloxham209@gmail.com>
2024-06-21 16:25:39 -0500

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { SignInButton } from "../components/sign-in-button";
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      <div className="home-page">
+        <h2>Home</h2>
+        <div>
+          <SignInButton className="signin-button" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
